c10ca39a46eff101a3f20eafa5e96b08c50ec1d3
Kaan-PC <83302633+kaplank1337@users.noreply.github.com>
2022-03-18 11:01:09 +0100

    firstday

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const cors = require('cors');
+const app = express()
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+let user = [];
+
+
+user.push({    
+        username: "Kaan",
+        password: "Kaan"
+})
+
+//funktioniert
+app.post('/login', function (req,res){
+    let username = req.body.username;
+    let password = req.body.password;
+
+    for(const element of user){
+        if(username == element.username && password == element.password){
+            return res.sendStatus(200);
+        }
+    }
+    return res.sendStatus(404);
+
+    
+})
+
+
+
+//funktioniert
+/*
+app.get('/', function (req, res) {
+  res.send(personen)
+})
+*/
+
+/*
+//funktioniert
+app.get('/:id', function (req,res){
+    
+    let personenid = req.params.id;
+
+    for(let i = 0;personen.length;i++){
+        if(personen[i].id == personenid){
+            res.send(personen[i]);
+        }
+    }
+    
+    
+})
+*/
+
+//Funktioniert
+/*app.delete('/:id', function (req,res){
+    let personenid = req.params.id;
+
+    for(let i = 0;personen.length;i++){
+        if(personen[i].id == personenid){
+            res.send("Benutzer: " + personen[i].name +" wurde gelöscht!");
+            personen.splice(i,1);            
+        }
+    }
+})
+*/
+
+
+
+
+
+/*
+//funktioniert
+app.put('/:id', function (req,res){
+    
+    let personenid = req.body.id;
+    let personenname = req.body.name;
+    let counter = 0;
+    
+
+    for(let i = 0;personen.length;i++){
+        if(personen[i].id == personenid){
+           personen[i].name = personenname;
+           res.status(200).send("Update wurde durchgeführt!")
+        } else{
+            counter++;           
+        } 
+        if(counter == personen.length){
+            personen.push({
+                id: personenid,
+                name: personenname
+            })
+            res.status(200).send("Benutzer wurde erstellt!");
+        }        
+    }
+})
+*/
+
+
+
+/*
+//funktioniert
+app.patch('/:id', function(req,res){
+    let entwurf = req.url;
+
+    let personenid = entwurf.substring(1);
+    let personenname = req.body.name;
+    //console.log(personenid);
+
+    for(let i = 0;personen.length;i++){
+        if(personen[i].id == personenid){
+            personen[i].name = personenname;
+            res.send("Benutzer wurde durch PATCH updated!")
+        }
+    }
+
+})
+*/
+
+
+app.listen(port)
